5d804a079c572d841fd17932eaec8dd00ce2f28a
Riccardo Masserini <masserini.riccardo.studente@itispaleocapa.it>
2024-05-23 10:19:51 +0200

    miglioramenti

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -51,14 +51,27 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(861, 3);
+            this.listView1.Location = new System.Drawing.Point(751, 3);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(620, 295);
+            this.listView1.Size = new System.Drawing.Size(730, 295);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.List;
@@ -66,7 +79,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 52);
+            this.label1.Location = new System.Drawing.Point(70, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 1;
@@ -75,7 +88,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(127, 52);
+            this.label2.Location = new System.Drawing.Point(180, 52);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 2;
@@ -84,7 +97,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(226, 52);
+            this.label3.Location = new System.Drawing.Point(284, 52);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(82, 13);
             this.label3.TabIndex = 3;
@@ -93,7 +106,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(348, 52);
+            this.label4.Location = new System.Drawing.Point(404, 52);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(51, 13);
             this.label4.TabIndex = 4;
@@ -101,28 +114,28 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(6, 68);
+            this.textBox1.Location = new System.Drawing.Point(60, 68);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(112, 68);
+            this.textBox2.Location = new System.Drawing.Point(166, 68);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 6;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(218, 68);
+            this.textBox3.Location = new System.Drawing.Point(272, 68);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 7;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(324, 68);
+            this.textBox4.Location = new System.Drawing.Point(378, 68);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 8;
@@ -130,7 +143,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(236, 27);
+            this.label5.Location = new System.Drawing.Point(288, 29);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(62, 13);
             this.label5.TabIndex = 9;
@@ -138,7 +151,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(54, 94);
+            this.button1.Location = new System.Drawing.Point(112, 94);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 10;
@@ -148,7 +161,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(372, 94);
+            this.button2.Location = new System.Drawing.Point(430, 94);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 23);
             this.button2.TabIndex = 11;
@@ -158,7 +171,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(430, 68);
+            this.textBox5.Location = new System.Drawing.Point(484, 68);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 12;
@@ -166,7 +179,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(438, 52);
+            this.label6.Location = new System.Drawing.Point(492, 52);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(80, 13);
             this.label6.TabIndex = 13;
@@ -174,7 +187,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(160, 94);
+            this.button3.Location = new System.Drawing.Point(218, 94);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 23);
             this.button3.TabIndex = 14;
@@ -184,7 +197,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(266, 94);
+            this.button4.Location = new System.Drawing.Point(324, 94);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 23);
             this.button4.TabIndex = 15;
@@ -195,9 +208,9 @@
             // listView2
             // 
             this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(861, 318);
+            this.listView2.Location = new System.Drawing.Point(751, 304);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(620, 281);
+            this.listView2.Size = new System.Drawing.Size(730, 324);
             this.listView2.TabIndex = 16;
             this.listView2.UseCompatibleStateImageBehavior = false;
             this.listView2.View = System.Windows.Forms.View.List;
@@ -250,11 +263,137 @@
             this.label9.TabIndex = 22;
             this.label9.Text = "Età";
             // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(430, 199);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 23;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(324, 199);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 24;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(452, 183);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Categoria";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(353, 183);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(36, 13);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "Sesso";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(536, 199);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 27;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(536, 183);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(100, 13);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "Numero di Pettorale";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(288, 152);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(78, 13);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "Profilo Sciatore";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(112, 225);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 23);
+            this.button5.TabIndex = 30;
+            this.button5.Text = "Aggiunta";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(218, 225);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 23);
+            this.button6.TabIndex = 31;
+            this.button6.Text = "Rimozione";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(324, 225);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 23);
+            this.button7.TabIndex = 32;
+            this.button7.Text = "Modifica";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(430, 225);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 23);
+            this.button8.TabIndex = 33;
+            this.button8.Text = "Stampa";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(642, 199);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 34;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(649, 183);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(83, 13);
+            this.label14.TabIndex = 35;
+            this.label14.Text = "Codice Univoco";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1493, 638);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -311,6 +450,19 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label14;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,16 +5,22 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static CapoLavoro2.Form1;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CapoLavoro2
 {
     public partial class Form1 : Form
     {
         private GestionePiste gestionePiste = new GestionePiste();
+        private GestioneSciatori gestioneSciatori = new GestioneSciatori();
+        private float tempoMassimo;
+        private float tempoMinimo;
+        private float tempoDiscesa;
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +30,7 @@ namespace CapoLavoro2
         {
 
         }
+        #region Classi
         public class Pista
         {
             public float TempoMassimo { get; set; }
@@ -41,6 +48,34 @@ namespace CapoLavoro2
                 Codice = codice;
             }
         }
+        public class Sciatore
+        {
+            public string Nome { get; set; }
+            public string Cognome { get; set; }
+            public int Eta { get; set; }
+            public string Categoria { get; set; }
+            public int NumeroPettorale { get; set; }
+            public string Sesso { get; set; }
+            public float TempoDiscesa { get; set; }
+            public int CodicePista { get; set; }
+            public int Punti { get; set; }
+
+            public Sciatore(string nome, string cognome, int eta, string categoria, int numeroPettorale, string sesso, float tempoDiscesa,int codicePista,int punti)
+            {
+                Nome = nome;
+                Cognome = cognome;
+                Eta = eta;
+                Categoria = categoria;
+                NumeroPettorale = numeroPettorale;
+                Sesso = sesso;
+                TempoDiscesa = tempoDiscesa; 
+                CodicePista = codicePista;
+                Punti = punti;
+            }
+        }
+        #endregion
+
+        #region GestionePiste
         public class GestionePiste
         {
             private List<Pista> piste = new List<Pista>();
@@ -76,7 +111,47 @@ namespace CapoLavoro2
             }
 
         }
+        #endregion
 
+        #region GestioneSciatori
+        public class GestioneSciatori
+        {
+            private List<Sciatore> sciatori = new List<Sciatore>();
+
+            public List<Sciatore> Sciatori
+            {
+                get { return sciatori; }
+            }
+
+            public void AggiungiSciatore(Sciatore sciatore)
+            {
+                sciatori.Add(sciatore);
+            }
+
+            public void RimuoviSciatore(int numeroPettorale)
+            {
+                var sciatoreDaRimuovere = sciatori.FirstOrDefault(s => s.NumeroPettorale == numeroPettorale);
+                if (sciatoreDaRimuovere != null)
+                {
+                    sciatori.Remove(sciatoreDaRimuovere);
+                }
+            }
+
+            public void ModificaSciatore(Sciatore sciatoreModificato)
+            {
+                var sciatoreDaModificare = sciatori.FirstOrDefault(s => s.NumeroPettorale == sciatoreModificato.NumeroPettorale);
+                if (sciatoreDaModificare != null)
+                {
+                    sciatoreDaModificare.Nome = sciatoreModificato.Nome;
+                    sciatoreDaModificare.Cognome = sciatoreModificato.Cognome;
+                    sciatoreDaModificare.Eta = sciatoreModificato.Eta;
+                    sciatoreDaModificare.Categoria = sciatoreModificato.Categoria;
+                    sciatoreDaModificare.Sesso = sciatoreModificato.Sesso;
+                }
+            }
+        }
+        #endregion
+        #region Buttons
         private void button1_Click(object sender, EventArgs e)
         {
             // Crea una nuova pista dai valori dei TextBox
@@ -91,6 +166,8 @@ namespace CapoLavoro2
             if (textBox5.Text.Length == 6)
             {
                 gestionePiste.AggiungiPista(pista);
+                tempoMassimo = float.Parse(textBox1.Text);
+                tempoMinimo = float.Parse(textBox2.Text);
 
                 MessageBox.Show("Pista aggiunta correttamente");
                 textBox1.Clear();
@@ -104,6 +181,7 @@ namespace CapoLavoro2
                 MessageBox.Show("Il codice deve essere di 6 cifre.");
             }
         }
+    
         private void button3_Click(object sender, EventArgs e)
         {
             // Prendi il codice dalla TextBox e convertilo in un intero
@@ -159,6 +237,181 @@ namespace CapoLavoro2
             textBox3.Clear();
             textBox4.Clear();
             textBox5.Clear();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            // Assumendo che tu abbia TextBox con questi nomi nel tuo form
+            string nome = textBox6.Text;
+            string cognome = textBox7.Text;
+            int eta = int.Parse(textBox8.Text);
+            string categoria = textBox9.Text;
+            int numeroPettorale = int.Parse(textBox11.Text);
+            string sesso = textBox10.Text;
+            int codicePista = int.Parse(textBox12.Text);
+            int punti = 0;
+
+            // Genera un tempo di discesa casuale tra 30 e 120 secondi
+            
+            Random random = new Random();
+            float tempoDiscesa = random.Next(int.Parse(tempoMinimo.ToString()), int.Parse(tempoMassimo.ToString()));
+
+            
+                Sciatore sciatore = new Sciatore(nome, cognome, eta, categoria, numeroPettorale, sesso, tempoDiscesa,codicePista,punti);
+                gestioneSciatori.AggiungiSciatore(sciatore);
+
+                MessageBox.Show("Sciatore aggiunto correttamente");
+                textBox6.Clear();
+                textBox7.Clear();
+                textBox8.Clear();
+                textBox9.Clear();
+                textBox10.Clear();
+                textBox11.Clear();
+                textBox12.Clear();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Assumendo che tu abbia un TextBox con questo nome nel tuo form
+            int numeroPettorale = int.Parse(textBox11.Text);
+
+            gestioneSciatori.RimuoviSciatore(numeroPettorale);
+
+            textBox11.Clear();
+            MessageBox.Show("Sciatore rimosso correttamente");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // Crea un nuovo sciatore dai valori dei TextBox
+            string nome = textBox6.Text;
+            string cognome = textBox7.Text;
+            int eta = int.Parse(textBox8.Text);
+            string categoria = textBox9.Text;
+            int numeroPettorale = int.Parse(textBox11.Text);
+            string sesso = textBox10.Text;
+            int codicePista = int.Parse(textBox12.Text);
+            int punti = 0;
+
+            if (textBox12.Text.Length == 6)
+            {
+                // Modifica lo sciatore
+
+                Sciatore sciatore = new Sciatore(nome, cognome, eta, categoria, numeroPettorale, sesso, tempoDiscesa, codicePista, punti);
+                gestioneSciatori.ModificaSciatore(sciatore);
+
+                MessageBox.Show("Sciatore modificato correttamente");
+                textBox6.Clear();
+                textBox7.Clear();
+                textBox8.Clear();
+                textBox9.Clear();
+                textBox10.Clear();
+                textBox11.Clear();
+                textBox12.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Il codice gara deve avere almeno 6 cifre");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+            listView2.Columns.Clear();
+            listView2.View = View.Details;
+            listView2.Columns.Add("Nome").Width = 120;
+            listView2.Columns.Add("Cognome").Width = 120;
+            listView2.Columns.Add("Età").Width = 120;
+            listView2.Columns.Add("Categoria").Width = 120;
+            listView2.Columns.Add("Numero Pettorale").Width = 120;
+            listView2.Columns.Add("Sesso").Width = 120;
+            listView2.Columns.Add("Tempo Discesa").Width = 120;
+            listView2.Columns.Add("Codice Pista").Width = 120;
+            foreach (var sciatore in gestioneSciatori.Sciatori)
+            {
+                ListViewItem item = new ListViewItem(sciatore.Nome);
+                item.SubItems.Add(sciatore.Cognome);
+                item.SubItems.Add(sciatore.Eta.ToString());
+                item.SubItems.Add(sciatore.Categoria);
+                item.SubItems.Add(sciatore.NumeroPettorale.ToString());
+                item.SubItems.Add(sciatore.Sesso);
+                item.SubItems.Add(sciatore.TempoDiscesa.ToString());
+                item.SubItems.Add(sciatore.CodicePista.ToString());
+
+                listView2.Items.Add(item);
+            }
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // Ordina la lista di sciatori in base al tempo di discesa (in ordine ascendente)
+            var sciatoriOrdinati = gestioneSciatori.Sciatori.OrderBy(s => s.TempoDiscesa).ToList();
+
+            // Assegna i punti in base alla posizione
+            for (int i = 0; i < sciatoriOrdinati.Count; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        sciatoriOrdinati[i].Punti = 25;
+                        break;
+                    case 1:
+                        sciatoriOrdinati[i].Punti = 18;
+                        break;
+                    case 2:
+                        sciatoriOrdinati[i].Punti = 15;
+                        break;
+                    case 3:
+                        sciatoriOrdinati[i].Punti = 12;
+                        break;
+                    case 4:
+                        sciatoriOrdinati[i].Punti = 10;
+                        break;
+                    case 5:
+                        sciatoriOrdinati[i].Punti = 8;
+                        break;
+                    case 6:
+                        sciatoriOrdinati[i].Punti = 6;
+                        break;
+                    case 7:
+                        sciatoriOrdinati[i].Punti = 4;
+                        break;
+                    case 8:
+                        sciatoriOrdinati[i].Punti = 2;
+                        break;
+                    case 9:
+                        sciatoriOrdinati[i].Punti = 1;
+                        break;
+                    default:
+                        sciatoriOrdinati[i].Punti = 0;
+                        break;
+                }
+            }
+
+
+            // Aggiorna la visualizzazione
+            listView2.Items.Clear();
+            listView2.Columns.Clear();
+            listView2.View = View.Details;
+            listView2.Columns.Add("Posizione").Width = 70;
+            listView2.Columns.Add("Nome").Width = 120;
+            listView2.Columns.Add("Cognome").Width = 120;
+            listView2.Columns.Add("Tempo Discesa").Width = 120;
+            listView2.Columns.Add("Punti").Width = 70;
+            for (int i = 0; i < sciatoriOrdinati.Count; i++)
+            {
+                ListViewItem item = new ListViewItem((i + 1).ToString());
+                item.SubItems.Add(sciatoriOrdinati[i].Nome);
+                item.SubItems.Add(sciatoriOrdinati[i].Cognome);
+                item.SubItems.Add(sciatoriOrdinati[i].TempoDiscesa.ToString());
+                item.SubItems.Add(sciatoriOrdinati[i].Punti.ToString());
+
+                listView2.Items.Add(item);
+            }
+
+            MessageBox.Show("Classifica aggiornata correttamente");
         }
     }
 }
+#endregion
